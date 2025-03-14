939c8608343d53cbf99f7b7f6c0e1f368c929f26
store-com <77104666+store-com@users.noreply.github.com>
2025-03-14 16:26:43 +0000

    Add files via upload

---
+++ freestore/spyphone/js/live.js
@@ -0,0 +1,13 @@
+var cp = "Likes";
+var codcp = ['1,500','1,500','3,000','6,000','3,000','10,000','3,000','1,500','6,000','1,500'];	
+var names = ['amiraX5','hindo02','kittyparty','smasher','xoxo0232','George', 'Smith', 'Sophia', 'Susan', 'Harry', 'Emily', 'Joe', 'Samantha', 'Lauren', 'Thomas', 'David', 'Robert','Jack', 'Charlie', 'Jacob', 'Alejandra', 'William', 'Amanda', 'Mark', 'Amelia', 'Kaylee', 'Michael', 'James', 'Olivia', 'Elizabeth', 'Connor', 'Victoria', 'Linda', 'Brown' ,'Ethan', 'Kyle', 'William', 'Oliver'];	
+function create() {
+VanillaToasts.create({
+  title: document.body.valueToUse = names[Math.floor(Math.random() * names.length)] || null,
+  text: document.body.valueToUse = codcp[Math.floor(Math.random() * codcp.length)] + cp,
+  type: 'success',
+  icon: 'img/person.jpg',
+  timeout: 2500,
+});
+setTimeout(create, 6000)
+}
